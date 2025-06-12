38a83258ffb30388954272f9ae9ee3cfa2eb16b7
Yohanes Rakinaung <johanesrakinaung@gmail.com>
2025-06-12 15:37:43 +0800

    Menghapus file konfigurasi, negara, dan paging yang tidak lagi digunakan untuk menyederhanakan kode dan meningkatkan pemeliharaan.

--- src/configs.js
+++
@@ -1,8 +0,0 @@
-const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDUyZDdkNWQzOGY1NzcyZTJiZDIzNWU2OWNkMzgwNiIsIm5iZiI6MTczMjA2MzcyNC40NTY0NTUyLCJzdWIiOiI2NzNjM2YyMjI0ODViODViM2NhOGNkMDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.B6BV6DpV28JJ6MUzmzdNgiEHq1Rr3pPbXIFuV5va4Zk", // Replace with your actual TMDB API token
-    },
-  };
--- src/paging.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    const searchParams = new URLSearchParams(window.location.search);
-    if(searchParams.get('genre') == null){
-      window.location = '?genre=';
-    }else if(searchParams.get('genre') == ''){
-    }else{
-      $('#searchDiv').remove();
-    }
-});
